0a823082c6df0aa4678869571df5792557d510bc
Marakuya90 <zhilzhil@mail.ru>
2022-10-20 09:53:05 +0300

    Vue randomfox

--- js/main.js
+++ js/main.js
@@ -1,22 +1,44 @@
-async function update(){
-    let responce = await fetch('https://randomfox.ca/floof/');
-    console.log(responce);
-    let result = await responce.json();
-    console.log(result);
-    let pos = result.image.lastIndexOf('.');
-    let ext = result.image.slice(pos + 1);
-    console.log(ext);
-    if (ext == 'mp4') {
-        document.querySelector('video').style.display = 'initial';
-        document.querySelector('img').style.display = 'none';
-        document.querySelector('video').src = result.image
+// async function update(){
+//     let responce = await fetch('https://randomfox.ca/floof/');
+//     console.log(responce);
+//     let result = await responce.json();
+//     console.log(result);
+//     let pos = result.image.lastIndexOf('.');
+//     let ext = result.image.slice(pos + 1);
+//     console.log(ext);
+//     if (ext == 'mp4') {
+//         document.querySelector('video').style.display = 'initial';
+//         document.querySelector('img').style.display = 'none';
+//         document.querySelector('video').src = result.image
 
-    } else {
-        document.querySelector('img').style.display = 'initial';
-        document.querySelector('video').style.display = 'none';
-        document.querySelector('img').src = result.image;
+//     } else {
+//         document.querySelector('img').style.display = 'initial';
+//         document.querySelector('video').style.display = 'none';
+//         document.querySelector('img').src = result.image;
+//     }
+// }
+// document.querySelector('button').addEventListener('click', update);
+
+// update();
+
+const app = Vue.createApp({
+    data() {
+        return {
+            url: "",
+            ext: ""
+        }
+    },
+    methods: {
+        update: async function () {
+            let responce = await fetch('https://randomfox.ca/floof/')
+            console.log(responce)
+            let result = await responce.json()
+            console.log(result);
+            let pos = result.image.lastIndexOf('.')
+            this.url = result.image
+            this.ext = result.image.slice(pos + 1)
+        }
     }
-}
-document.querySelector('button').addEventListener('click', update);
+})
 
-update();
+app.mount('main')
